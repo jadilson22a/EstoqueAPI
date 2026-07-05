737ce9888e1e6731bba5955c4cc7632d4c9681aa
jadilson22a <jadilsonalves777.ma@outlook.com>
2026-07-04 21:39:43 -0300

    add validação produtoDTO

--- src/main/java/com/github/jadilson22a/estoqueapi/entities/ProdutoDTO.java
+++ src/main/java/com/github/jadilson22a/estoqueapi/entities/ProdutoDTO.java
@@ -1,8 +1,20 @@
 package com.github.jadilson22a.estoqueapi.entities;
 
 import com.github.jadilson22a.estoqueapi.models.Produto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 
-public record ProdutoDTO(Integer id, String nome, Double quantidade, UnidadeMedida unidadeMedida) {
+public record ProdutoDTO(
+        Integer id,
+
+        @NotBlank(message = "nome é obrigatorio")
+        String nome,
+
+        @PositiveOrZero(message = "O numero precisa ser positivo ou zero")
+        Double quantidade,
+
+        @NotBlank(message = "unidade de medida é obrigatorio")
+        UnidadeMedida unidadeMedida) {
 
     public Produto toProduto(){
         return new Produto(id, nome, quantidade, unidadeMedida);
